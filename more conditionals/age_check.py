d6d56b08162908068c092ee7d79f1bc7d91bfbbb
sernamalala <sm.sernamalala@gmail.com>
2025-12-20 21:25:33 +0200

    age check code

---
+++ more conditionals/age_check.py
@@ -0,0 +1,12 @@
+# Write your solution here
+
+age = int(input("What is your age? "))
+
+if age<5 and age>=0:
+    print("I suspect you can't write quite yet...")
+
+elif age>=5 and age<=125:
+    print(f"Ok, you're {age} years old")
+
+else:
+    print("That must be a mistake")
