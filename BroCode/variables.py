6339d88daa415a1571f65b616b575c25613e4020
sernamalala <sm.sernamalala@gmail.com>
2025-12-26 20:36:58 +0200

    brocode videos tutorials

---
+++ BroCode/variables.py
@@ -0,0 +1,21 @@
+# Variables
+
+first_name = "Serna"
+last_name = "Malala"
+
+print(first_name)
+
+# casting
+
+age = 22
+student= False
+
+print(type(first_name))
+
+age = float(age)
+
+print(age)
+
+print(str(student))
+
+print(bool(age))
