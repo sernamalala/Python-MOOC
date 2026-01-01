d0e5c9263d9f5e1e280dc3ec1e50afdadd242d59
sernamalala <sm.sernamalala@gmail.com>
2026-01-01 15:59:03 +0200

    Started with JS exercises again to reinforce JS concepts

---
+++ BroCode/user_input.py
@@ -0,0 +1,9 @@
+#Are of a rec
+
+l = float(input("Enter the length of a rectangle: "))
+w = l = float(input("Enter the width of a rectangle: "))
+
+area = l*w
+
+print(f"The area of the ractangle is {area} cm^2")
+
---
+++ JS/modulefive.js
@@ -0,0 +1,37 @@
+// Exercise 1
+
+let items = [
+  {
+    id: "tom125",
+    name: "Tomato",
+    qty: 5,
+    category: "fruit",
+  },
+  {
+    id: "cheese345",
+    name: "Cheese",
+    qty: 1,
+    category: "dairy",
+  },
+  {
+    id: "dough1278",
+    name: "Dough",
+    qty: 3,
+    category: "carbs",
+  },
+  {
+    id: "pep359",
+    name: "Pepperoni",
+    qty: 25,
+    category: "meat",
+  },
+  {
+    id: "oil904",
+    name: "Avocado Oil",
+    qty: 1,
+    category: "fats and oils",
+  },
+];
+
+console.log(items.length);
+console.log(items);
