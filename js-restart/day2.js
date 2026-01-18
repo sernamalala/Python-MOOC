6213e842af0b4158b47693aea3185b866d4ed956
sernamalala <sm.sernamalala@gmail.com>
2026-01-18 20:16:27 +0200

    started with weather app project

---
+++ js-restart/day2.js
@@ -0,0 +1,27 @@
+console.log("5" == 5);
+//false
+console.log("5" === 5);
+//true
+
+console.log(null == undefined);
+//false
+console.log(null === undefined);
+//true
+
+console.log(0 == false);
+//true
+console.log(0 === false);
+//true
+
+console.log("" == false);
+console.log("" === false);
+//true
+
+console.log(" " == false);
+console.log(" " === false);
+//true
+
+console.log("0" == false);
+//true
+console.log("0" === false);
+//true
