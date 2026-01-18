33b97cb6822cd4a10506004327d8a6b337deae62
sernamalala <sm.sernamalala@gmail.com>
2026-01-18 19:26:47 +0200

    types in JS

---
+++ js-restart/day1.js
@@ -0,0 +1,18 @@
+let name = "Serna";
+let age = 22;
+let student = true;
+let money = null;
+let opts = undefined;
+let obj = {
+  firstName: "Serna",
+  lastName: "Malala",
+};
+let array = [name, age, student];
+
+console.log(name, typeof name);
+console.log(age, typeof age);
+console.log(student, typeof student);
+console.log(money, typeof money);
+console.log(opts, typeof opts);
+console.log(obj, typeof obj);
+console.log(array, typeof array);
