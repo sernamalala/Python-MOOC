26260b3c49a50c7d802e7af055e5f66dbd3f89b1
sernamalala <sm.sernamalala@gmail.com>
2026-02-19 19:18:33 +0200

    completed all normal exercises for day 2

--- js-restart/19Feb26.js
+++ js-restart/19Feb26.js
@@ -19,6 +19,7 @@ console.log(`[${filterNum}]`);
 
 //Core Ex1
 //reduce
+//Use .reduce() to calculate the total price.
 let cartPrices = [200, 150, 400, 100];
 
 let total = cartPrices.reduce((acc, element) => {
@@ -29,6 +30,7 @@ console.log(total);
 
 //Core Ex2
 //objects + reduce
+//Use reduce to calculate total age of all users.
 
 let users = [
   { name: "Serna", age: 22 },
@@ -37,6 +39,37 @@ let users = [
   { name: "PP", age: 27 },
 ];
 
-let totalAge;
+let totalAge = users.reduce((accumulator, current) => {
+  return accumulator + current.age;
+}, 0);
 
-console.log(totalAge);
+console.log("The total age is " + totalAge);
+
+//Core 3
+/*
+Do all of this in ONE chain:
+
+1. Filter products above 1000
+2. Add 10% tax to each price
+3. Get the total price of those taxed products
+*/
+
+let products = [
+  { name: "Laptop", price: 5000 },
+  { name: "Phone", price: 2000 },
+  { name: "TV", price: 3000 },
+  { name: "Mouse", price: 500 },
+];
+
+let myProducts = products
+  .filter((element) => {
+    return element.price > 1000;
+  })
+  .map((element) => {
+    return element.price * 1.1;
+  })
+  .reduce((accumulator, current) => {
+    return accumulator + current;
+  }, 0);
+
+console.log("The total products price: " + myProducts);
