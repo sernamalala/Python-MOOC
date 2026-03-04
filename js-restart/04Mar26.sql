bec4b16207cdb16cad52f773bf4a477d221c4276
sernamalala <sm.sernamalala@gmail.com>
2026-03-04 18:04:54 +0200

    SQL practice ex batch 2

--- js-restart/04Mar26.sql
+++ js-restart/04Mar26.sql
@@ -35,3 +35,31 @@ FROM
     OCRD T0
 ORDER BY
     T0."UpdateDate" DESC
+    --Exercise 5 --Show Top 10 Customers ordered by CardName A–Z
+SELECT
+    TOP 10 T0."CardCode",
+    T0."CardName"
+FROM
+    OCRD T0
+WHERE
+    T0."CardType" = 'C'
+ORDER BY
+    T0."CardName"
+    --Exercise 6 --Show all BPs where CardName contains “Tech”
+SELECT
+    T0."CardCode",
+    T0."CardName"
+FROM
+    OCRD T0
+WHERE
+    T0."CardName" LIKE '%Tech%'
+    --Exercise 7 --Show BPs updated between 2026-03-01 and 2026-03-04
+SELECT
+    T0."CardCode",
+    T0."CardName",
+    T0."UpdateDate"
+FROM
+    OCRD T0
+WHERE
+    T0."UpdateDate" <= '2026-03-04'
+    AND T0."UpdateDate" >= '2026-03-01'
