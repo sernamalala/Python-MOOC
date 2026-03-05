8be3994e5e8a2c27f914fa8588ac62ee7c202373
sernamalala <sm.sernamalala@gmail.com>
2026-03-05 08:37:30 +0200

    Continuing with SQL practice pt 3

--- js-restart/05Mar26.sql
+++ js-restart/05Mar26.sql
@@ -39,3 +39,16 @@ GROUP BY
     T1."PymntGroup"
 ORDER BY
     'CustomerCount' DESC
+    -- Exercise 11 -- same as Ex 10 but for Vendors instead of Customers.
+SELECT
+    T1."PymntGroup" AS 'Payment Term Name',
+    COUNT(T0."CardCode") AS 'VendorCount'
+FROM
+    OCRD T0
+    INNER JOIN OCTG T1 ON T0."GroupNum" = T1."GroupNum"
+WHERE
+    T0."CardType" = 'S'
+GROUP BY
+    T1."PymntGroup"
+ORDER BY
+    VendorCount DESC
