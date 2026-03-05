5ad550b95775edfeb183762b29069f323b70398e
sernamalala <sm.sernamalala@gmail.com>
2026-03-05 08:13:51 +0200

    Continuing with SQL practice pt 2

--- js-restart/05Mar26.sql
+++ js-restart/05Mar26.sql
@@ -11,3 +11,31 @@ WHERE
     T0."CardType" = 'C'
 ORDER BY
     T0."UpdateDate" DESC
+    -- Exercise 9
+SELECT
+    T0."CardCode",
+    T0."CardName",
+    T3."PymntGroup" as 'Payment Term Name',
+    T0."UpdateDate",
+    T1."U_NAME"
+FROM
+    OCRD T0
+    INNER JOIN OUSR T1 ON T0."UserSign2" = T1."USERID"
+    INNER JOIN OCTG T3 ON T0."GroupNum" = T3."GroupNum"
+WHERE
+    T0."CardType" = 'C'
+ORDER BY
+    T0."CardName"
+    -- Exercise 10 --Show how many Customers are in each Payment Term.
+SELECT
+    T1."PymntGroup" AS 'Payment Term Name',
+    COUNT(T0."CardName") AS 'CustomerCount'
+FROM
+    OCRD T0
+    INNER JOIN OCTG T1 ON T0."GroupNum" = T1."GroupNum"
+WHERE
+    T0."CardType" = 'C'
+GROUP BY
+    T1."PymntGroup"
+ORDER BY
+    'CustomerCount' DESC
